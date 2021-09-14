6f2a1aa44b57b77cf5318171a1a6eb81e73b54e0
jiohc <85597770+jiohc@users.noreply.github.com>
2021-09-13 20:44:43 -0700

    add time blocks and js

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,8 +12,6 @@ function timeBlockColor() {
     $(".time-block").each(function() {
         var currHour = parseInt($(this).attr("id"));
 
-        // console.log(this); //each time-block
-
         if (currHour > hour) {
             $(this).addClass("future");
         } else if (currHour === hour) {
@@ -24,27 +22,23 @@ function timeBlockColor() {
     })
 };
 
-// WHEN I click the save button for that time block
+// save button for that time block
 saveBtn.on("click", function() {
-
-    // console.log(this); //save button
+    
     var time = $(this).siblings(".hour").text();
     var plan = $(this).siblings(".plan").val();
 
-    // THEN the text for that event is saved in local storage
+    // event is saved in local storage
     localStorage.setItem(time, plan);
 });
 
-// WHEN I refresh the page
-// THEN the saved events persist
+
+// saved events
 function usePlanner() {
 
     $(".hour").each(function() {
         var currHour = $(this).text();
         var currPlan = localStorage.getItem(currHour);
-
-        // console.log(this);
-        // console.log(currHour);
 
         if(currPlan !== null) {
             $(this).siblings(".plan").val(currPlan);
